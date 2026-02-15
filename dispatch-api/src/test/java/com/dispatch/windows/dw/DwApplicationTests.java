79f1f7b23edfe7fe73055f4d108076b72c74ff4b
Jose Gonzalez <josegonzalez293@gmail.com>
2026-02-15 12:13:56 -0300

    first commit: init api

---
+++ dispatch-api/src/test/java/com/dispatch/windows/dw/DwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispatch.windows.dw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
